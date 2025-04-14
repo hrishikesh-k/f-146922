bcded43b3dd1b030f30fcc81b8cf41716fe567a3
unknown <lucasmendoncawf@gmail.com>
2025-04-14 16:06:40 -0300

    fix issue

--- src/app/components/pages/about/about.component.ts
+++ src/app/components/pages/about/about.component.ts
@@ -1,12 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { ProjectLinkComponent } from './project-link/project-link.component';
 import { SkillComponent } from '../../shared/skill/skill.component';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
-  imports: [ProjectLinkComponent, SkillComponent],
+  imports: [SkillComponent],
 })
 export class AboutComponent implements OnInit {
 
