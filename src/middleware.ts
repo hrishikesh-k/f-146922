d969b46250d5cd5ccbe0f2ea74f44373d6661e7d
unknown <lucasmendoncawf@gmail.com>
2025-05-05 16:02:13 -0300

    testing netlify

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,19 @@
-import { NextResponse, type NextRequest } from 'next/server';
-// import createMiddleware from 'next-intl/middleware';
+import type { NextRequest } from 'next/server';
+import createMiddleware from 'next-intl/middleware';
 
-// import { routing } from './i18n/routing';
+import { routing } from './i18n/routing';
 
-// const intlMiddleware = createMiddleware(routing);
+const intlMiddleware = createMiddleware(routing);
 
 export function middleware(request: NextRequest) {
-  const response = NextResponse.next();
-  // const responseIntl = intlMiddleware(request);
+  const response = intlMiddleware(request);
 
   const initialTheme = request.cookies.get('theme')?.value;
   let theme = 'light';
   if (initialTheme === 'dark') {
     theme = 'dark';
   }
+
   response.headers.set('x-theme', theme);
 
   return response;
